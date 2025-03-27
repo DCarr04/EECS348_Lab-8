b4fbe3303f4449b76e149a3e6215eea3cd7b4378
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-27 10:40:52 -0500

    added destructor and Matrix(vector<vector<int>> nums)

--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,6 @@
 #include "matrix.hpp"
+#include <vector>
+using namespace std;
 
 // implementation of functions declared in matrix.hpp goes here
 // function definitions for a class have their names prefixed with
@@ -10,6 +12,13 @@ Matrix::Matrix(size_t N) {
     // initialize an empty NxN matrix
 }
 
+Matrix::Matrix(vector<vector<int>> nums){
+    
+}
+
+Matrix::~Matrix(){
+
+}
 // e.g. for a member function:
 
 void Matrix::print_matrix() const {
