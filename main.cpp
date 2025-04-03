482b4549276cfc8cd0b00ea639e40738916cb60b
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-02 19:39:08 -0500

    added testFile and added cin in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include "matrix.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main(int argc, char *argv[]) {
+    string file;
+    cout << "Enter a file: ";
+    cin >> file;
+    ifstream myfile (file);
+
     return 0;
 }
