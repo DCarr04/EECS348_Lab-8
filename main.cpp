cdb4bfc72c7e06f92fed52e0662840ad9c940440
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-03 10:15:39 -0500

    copied for loop for getting data for Matrix1 into else statement to get data for Matrix2

--- main.cpp
+++ main.cpp
@@ -10,20 +10,21 @@ int main(int argc, char *argv[]) {
     string line;
     int rowCounter = 0;
     int data;
+    int size;
     int sizeTracker = 0;
     string preInt_data;
     cout << "Enter a file: ";
     cin >> file;
     ifstream myfile (file);
     vector<vector<int>> nums;
-
+    vector<vector<int>> nums2;
 
     if (myfile.is_open()){
         while(getline(myfile, line)){
             //Matrix myMatrix();
             if (line.size() == 1){
-                int size = stoi(line);
-                Matrix myMatrix(size);
+                size = stoi(line);
+                //Matrix myMatrix(size);
 
             
             if(sizeTracker < size){
@@ -34,12 +35,26 @@ int main(int argc, char *argv[]) {
                         nums[rowCounter][i] = data;
                         preInt_data.pop_back();
                     }
+                    ++rowCounter;
+                }
+            }else{
+                if (line.size() == size){
+                    for(int j = 0; j < size; j++){
+                        preInt_data.push_back(line[j]);
+                        data = stoi(preInt_data);
+                        nums2[rowCounter][j] = data;
+                        preInt_data.pop_back();
+                    }
+                    ++rowCounter;
+
                 }
             }
             
+            //myMatrix(nums);
             
             
         }
+        Matrix myMatrix(nums);
 
     }
 
