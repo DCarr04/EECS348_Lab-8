586fdbb394797ec86fe70a6d00c5e83af57c6232
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-03 09:49:35 -0500

    try to read from file for first matrix

--- main.cpp
+++ main.cpp
@@ -8,21 +8,36 @@ using namespace std;
 int main(int argc, char *argv[]) {
     string file;
     string line;
-    
+    int rowCounter = 0;
+    int data;
+    int sizeTracker = 0;
+    string preInt_data;
     cout << "Enter a file: ";
     cin >> file;
     ifstream myfile (file);
+    vector<vector<int>> nums;
+
+
     if (myfile.is_open()){
         while(getline(myfile, line)){
-            Matrix myMatrix();
+            //Matrix myMatrix();
             if (line.size() == 1){
                 int size = stoi(line);
-                myMatrix(size);
+                Matrix myMatrix(size);
 
-            vector<vector<int>> data;
-            for(int i = 0; i < size; i++){
-                
+            
+            if(sizeTracker < size){
+                if(line.size() == size){
+                    for(int i = 0; i < size; i++){
+                        preInt_data.push_back(line[i]);
+                        data = stoi(preInt_data);
+                        nums[rowCounter][i] = data;
+                        preInt_data.pop_back();
+                    }
+                }
             }
+            
+            
             
         }
 
