dfd9fecb1be3eb51141acf4c64908389c19c3729
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-02 21:30:13 -0500

    open file and create Matrix object- doesn't work

--- main.cpp
+++ main.cpp
@@ -6,9 +6,21 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     string file;
+    string line;
+    
     cout << "Enter a file: ";
     cin >> file;
     ifstream myfile (file);
+    if (myfile.is_open()){
+        while(getline(myfile, line)){
+            Matrix myMatrix();
+            if (line.size() == 1){
+                int size = stoi(line);
+                myMatrix(size);
+            }
+        }
+
+    }
 
     return 0;
 }
